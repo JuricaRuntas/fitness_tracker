4286fd1cd2930fb9487b3fcd928c98a0efbbefe6
Jurica Runtas <runtas.j@gmail.com>
2020-08-24 15:54:28 +0200

    main window

---
+++ fitness_tracker/fitness_tracker.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
+from PyQt5.QtCore import Qt
+from homepage import Homepage
+
+class FitnessTracker(QMainWindow):
+  def __init__(self):
+    super().__init__()
+    self.create_window()
+    self.cw = Homepage(self)
+    self.setCentralWidget(self.cw)
+  
+  def create_window(self):
+    self.setWindowTitle("Fitness Tracker")
+    self.resize(800, 600)
+    self.center()
+    self.show()
+  
+  def center(self):
+    window = self.frameGeometry()
+    center_point = QDesktopWidget().availableGeometry().center()
+    window.moveCenter(center_point)
+    self.move(window.topLeft())
+
+if __name__ == "__main__":
+  app = QApplication(sys.argv)
+  ft = FitnessTracker()
+  sys.exit(app.exec_())
