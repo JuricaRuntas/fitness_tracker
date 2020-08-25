975f1281b40781a9b4ece617c42a9a876d03459d
Jurica Runtas <runtas.j@gmail.com>
2020-08-25 17:26:19 +0200

    added fixed sizes to labels, update

--- fitness_tracker/homepage/side_panel.py
+++ fitness_tracker/homepage/side_panel.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QPushButton, QVBoxLayout
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtGui import QFont
 
 class SidePanel(QWidget):
   def __init__(self, parent):
@@ -15,7 +15,7 @@ class SidePanel(QWidget):
     notes_layout = QVBoxLayout()
     notes_label = QLabel("Notes", self)
     notes_label.setFont(QFont("Ariel", 15))
-    notes_label.setAlignment(Qt.AlignLeft)
+    notes_label.setFixedSize(100,20)
     lifts_button = QPushButton("Big Lifts", self)
     workouts_button = QPushButton("Workouts", self)
     calories_button = QPushButton("Calories", self)
@@ -24,7 +24,7 @@ class SidePanel(QWidget):
     calculators_layout = QVBoxLayout()
     calculators_label = QLabel("Calculators", self)
     calculators_label.setFont(QFont("Ariel", 15))
-    calculators_label.setAlignment(Qt.AlignLeft)
+    calculators_label.setFixedSize(100,20)
     max_button = QPushButton("1 Rep Max", self)
     fat_button = QPushButton("Body Fat", self)
     strength_es_button = QPushButton("Strength Estimator", self)
@@ -32,7 +32,7 @@ class SidePanel(QWidget):
     stats_layout = QVBoxLayout()
     stats_label = QLabel("Statistics", self)
     stats_label.setFont(QFont("Ariel", 15))
-    stats_label.setAlignment(Qt.AlignLeft)
+    stats_label.setFixedSize(100,20)
     strength_button = QPushButton("Strength", self)
     weight_loss_button = QPushButton("Weight Loss", self)
     
@@ -41,13 +41,10 @@ class SidePanel(QWidget):
                calculators_layout: [calculators_label, max_button, fat_button, strength_es_button],
                stats_layout: [stats_label, strength_button, weight_loss_button]}
     
-    for key, value in layouts.items():
+    grid = QGridLayout()
+    
+    for i, (key, value) in enumerate(layouts.items()):
       for widget in value:
         key.addWidget(widget)
-
-    grid = QGridLayout()
-    grid.addLayout(general_layout, 0, 0, 2, 1)
-    grid.addLayout(notes_layout, 3, 0, 8, 1)
-    grid.addLayout(calculators_layout, 12, 0, 7, 1)
-    grid.addLayout(stats_layout, 20, 0, 5, 1)
+      grid.addLayout(key, i, 0)
     self.setLayout(grid)
