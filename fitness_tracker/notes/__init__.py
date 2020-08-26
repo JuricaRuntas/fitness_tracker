4d3b3d2e95ee1d5daffe781d550cdc8b28b71398
Jurica Runtas <runtas.j@gmail.com>
2020-08-26 14:27:47 +0200

    imports

---
+++ fitness_tracker/notes/__init__.py
@@ -0,0 +1,4 @@
+from notes.big_lifts.big_lifts import BigLiftsNotes
+from notes.weight_loss.weight_loss import WeightLossNotes
+from notes.workouts.workouts import WorkoutsNotes
+from notes.calories.calories import CaloriesNotes
