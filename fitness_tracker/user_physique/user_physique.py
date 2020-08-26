918712e927021cae53865c24563bd7653343bfc6
Jurica Runtas <runtas.j@gmail.com>
2020-08-26 14:32:15 +0200

    user physique

---
+++ fitness_tracker/user_physique/user_physique.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtGui import QFont
+
+from homepage.side_panel import SidePanel
+
+class UserPhysique(QWidget):
+  def __init__(self, controller):
+    super().__init__()
+    self.controller = controller
+    label = QLabel("User Physique", self)
+    label.move(300, 300)
+    label.setFont(QFont("Ariel", 30))
+    self.side_panel = SidePanel(self, self.controller)
