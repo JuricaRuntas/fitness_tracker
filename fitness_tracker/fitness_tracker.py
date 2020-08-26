c0c080e11538eb7e74baab088aef131b49a80766
Jurica Runtas <runtas.j@gmail.com>
2020-08-26 14:10:21 +0200

    implemented layout switching

--- fitness_tracker/fitness_tracker.py
+++ fitness_tracker/fitness_tracker.py
@@ -2,6 +2,10 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
 
 import sys
 from homepage.homepage import Homepage
+from user_physique.user_physique import UserPhysique
+from calculators import *
+from notes import *
+from statistics import *
 
 class FitnessTracker(QMainWindow):
   def __init__(self):
@@ -9,6 +13,11 @@ class FitnessTracker(QMainWindow):
     self.create_window()
     self.cw = Homepage(self)
     self.setCentralWidget(self.cw)
+    self.layouts = {"Home": Homepage, "User Physique": UserPhysique, "Big Lifts": BigLiftsNotes,
+                    "Workouts": WorkoutsNotes, "Calories": CaloriesNotes,
+                    "Weight Loss Notes": WeightLossNotes, "1 Rep Max": OneRepMaxCalculator,
+                    "Body Fat": BodyFatCalculator, "Strength Estimator": StrengthEstimator,
+                    "Strength": StrengthStats, "Weight Loss": WeightLossStats}
   
   def create_window(self):
     self.setWindowTitle("Fitness Tracker")
@@ -21,6 +30,11 @@ class FitnessTracker(QMainWindow):
     center_point = QDesktopWidget().availableGeometry().center()
     window.moveCenter(center_point)
     self.move(window.topLeft())
+  
+  def display_layout(self, layout):
+    for key, value in self.layouts.items():
+      if layout == key:
+        self.setCentralWidget(value(self))
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
