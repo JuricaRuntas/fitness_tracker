1f8bd9a352a9543b47c274f0b1b125fbff457f01
Jurica Runtas <runtas.j@gmail.com>
2020-08-28 16:09:31 +0200

    update

--- fitness_tracker/calculators/body_fat/body_fat.py
+++ fitness_tracker/calculators/body_fat/body_fat.py
@@ -1,13 +1,22 @@
-from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel
 from PyQt5.QtGui import QFont
 
 from homepage.side_panel import SidePanel
+from homepage.header import Header
+from .main_panel import MainPanel
 
 class BodyFatCalculator(QWidget):
   def __init__(self, controller):
     super().__init__()
     self.controller = controller
-    label = QLabel("Body Fat Calculator", self)
-    label.move(300, 300)
-    label.setFont(QFont("Ariel", 30))
+    self.header = Header(self, "Body Fat Calculator")
+    self.main_panel = MainPanel(self)
     self.side_panel = SidePanel(self, self.controller)
+    self.create_grid()
+
+  def create_grid(self):
+    grid = QGridLayout()
+    grid.addWidget(self.header, 0, 1, 1, 4)
+    grid.addWidget(self.side_panel, 1, 1, 8, 1)
+    grid.addWidget(self.main_panel, 1, 2, 8, 3)
+    self.setLayout(grid)
