61cab3762fa589d63cc2a312d2c02c6166d4a2fe
Jurica Runtas <runtas.j@gmail.com>
2020-08-30 19:17:28 +0200

    calorie notes

--- fitness_tracker/notes/calories/calories.py
+++ fitness_tracker/notes/calories/calories.py
@@ -1,13 +1,22 @@
-from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout
 from PyQt5.QtGui import QFont
 
+from .main_panel import MainPanel
 from homepage.side_panel import SidePanel
+from .header import Header
 
 class CaloriesNotes(QWidget):
   def __init__(self, controller):
     super().__init__()
     self.controller = controller
-    label = QLabel("Calories", self)
-    label.move(300, 300)
-    label.setFont(QFont("Ariel", 30))
+    self.header = Header(self, "Calories Notes"       )
+    self.main_panel = MainPanel(self)
     self.side_panel = SidePanel(self, self.controller)
+    self.create_grid()
+
+  def create_grid(self):
+    grid = QGridLayout()
+    grid.addWidget(self.header, 0, 0, 1, 4)
+    grid.addWidget(self.side_panel, 1, 0, 8, 1)
+    grid.addWidget(self.main_panel, 1, 1, 8, 3)
+    self.setLayout(grid)
