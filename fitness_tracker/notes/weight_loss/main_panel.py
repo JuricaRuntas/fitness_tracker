1200a8561df32e1b13992e57eae84ad5da093c6e
Jurica Runtas <runtas.j@gmail.com>
2020-09-05 16:03:09 +0200

    weight loss notes main panel

---
+++ fitness_tracker/notes/weight_loss/main_panel.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QWidget
+
+class MainPanel(QWidget):
+  def __init__(self, parent):
+    super().__init__(parent)
+    self.create_panel()
+
+  def create_panel(self):
+    pass
+   
