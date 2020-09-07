ad26d8e2f0c4843d9eb26952f74b7810df074528
Jurica Runtas <runtas.j@gmail.com>
2020-09-07 07:33:31 +0200

    update workouts

--- fitness_tracker/notes/workouts/workouts.py
+++ fitness_tracker/notes/workouts/workouts.py
@@ -1,13 +1,22 @@
-from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel
 from PyQt5.QtGui import QFont
 
+from .header import Header
 from homepage.side_panel import SidePanel
+from .main_panel import MainPanel
 
 class WorkoutsNotes(QWidget):
   def __init__(self, controller):
     super().__init__()
     self.controller = controller
-    label = QLabel("Workouts", self)
-    label.move(300, 300)
-    label.setFont(QFont("Ariel", 30))
+    self.header = Header(self, "Workout Notes")
+    self.main_panel = MainPanel(self)
     self.side_panel = SidePanel(self, self.controller)
+    self.create_grid()
+
+  def create_grid(self):
+    grid = QGridLayout()
+    grid.addWidget(self.header, 0, 0, 1, 4)
+    grid.addWidget(self.side_panel, 1, 0, 8, 1)
+    grid.addWidget(self.main_panel, 1, 1, 8, 3)
+    self.setLayout(grid)
