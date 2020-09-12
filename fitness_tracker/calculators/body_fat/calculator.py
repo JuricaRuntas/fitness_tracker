b31fadd15829e43c6c38dcfb75095d108e0a644d
Jurica Runtas <runtas.j@gmail.com>
2020-09-12 16:55:27 +0200

    update calculator

--- fitness_tracker/calculators/body_fat/calculator.py
+++ fitness_tracker/calculators/body_fat/calculator.py
@@ -1,7 +1,7 @@
-import math
+from body_fat_formulas import *
 
 class Calculator:
-  def __init__(self, gender, age, weight, height, neck, waist, hip, units):
+  def __init__(self, gender, age, weight, height, neck, waist, units, hip=None):
     self.gender = gender
     self.age = age
     self.weight = weight
@@ -10,6 +10,7 @@ class Calculator:
     self.waist = waist
     self.hip = hip
     self.units = units
+    self.results = {}
     
     # Jackson & Pollard Ideal Body Fat Percentages
     self.ideal_fat_women = {20: "17.7%", 25: "18.4%", 30: "19.3%",
@@ -24,35 +25,66 @@ class Calculator:
     self.categorization_women = {"10-13%": "Essential Fat", "14-20%": "Athletes",
                                  "21-24%": "Fitness", "25-31%": "Average", "32+%": "Obese"}
 
-    self.categorization_men = {"2-5%": "Essential Fat", "6-13%": "Athletes",
+    self.categorization_men = {"02-05%": "Essential Fat", "06-13%": "Athletes",
                                "14-17%": "Fitness", "18-25%": "Average", "25+%": "Obese"}
 
-    self.calculate()
+    self.calculate_body_fat()
+    self.create_info()
 
-  def calculate(self):
-    if self.units == "inches":
+  def calculate_body_fat(self):
+    if self.units == "imperial":
       if self.gender == "male":
-        # U.S. Navy Method
-        usc_bfp = 86.010*math.log10(self.waist-self.neck)-70.041*math.log10(height)+36.76 
-        # BMI Method
-        BMI_usc = 703*(self.weight/self.height**2)
-        bmi_bfp = 1.20*BMI_usc+0.23*self.age-16.2
+        bf_navy_usc = male_navy_USC(self.waist, self.neck, self.height)
+        bf_BMI_usc = male_BMI_USC(self.weight, self.height, self.age)
       elif self.gender == "female":
-        # U.S Navy Method
-        usc_bfp = 163.205*math.log10(self.waist+self.hip-self.neck)-97.684*math.log10(self.height)+36.76
-        # BMI Method
-        BMI_usc = 703*(self.weight/self.height**2)
-        bmi_bfp = 1.20*BMI_usc+0.23*self.age-5.4
+        bf_navy_usc = female_navy_USC(self.waist, self.neck, self.height, self.hip)
+        bf_BMI_usc = female_navy_SI(self.waist, self.neck, self.height, self.hip)
+      
+      body_fat_mass = fat_mass(bf_navy_usc, self.weight)
+      lean_mass = lean_body_mass(self.weight, body_fat_mass)
+      
+      self.results["Body Fat Navy"] = round(bf_navy_usc, 1)
+      self.results["Body Fat BMI"] = round(bf_BMI_usc, 1)
+      self.results["Fat Mass"] = round(body_fat_mass, 1)
+      self.results["Lean Body Mass"] = round(lean_mass, 1)
+    
+    elif self.units == "metric":
+      if self.gender == "male":
+        bf_navy_si = male_navy_SI(self.waist, self.neck, self.height)
+        bf_BMI_si = male_BMI_SI(self.weight, self.height, self.age)
+      elif self.gender == "female":
+        bf_navy_si = female_navy_SI(self.waist, self.neck, self.height, self.hip)
+        bf_BMI_si = female_BMI_SI(self.weight, self.height, self.age)
+      
+      body_fat_mass = fat_mass(bf_navy_si, self.weight)
+      lean_mass = lean_body_mass(self.weight, body_fat_mass)
+      
+      self.results["Body Fat Navy"] = round(bf_navy_si, 1)
+      self.results["Body Fat BMI"] = round(bf_BMI_si, 1)
+      self.results["Fat Mass"] = round(body_fat_mass, 1)
+      self.results["Lean Body Mass"] = round(lean_mass, 1)
+
+  def create_info(self):
+    if self.gender == "male":
+      self.results["Ideal Body Fat"] = self.ideal_fat_men[self.age]
+    elif self.gender == "female": self.results["Ideal Body Fat"] = self.ideal_fat_women[self.age]
+    
+    for key, value in self.categorization_men.items():
+      if key[0] == "0":
+        if int(self.results["Body Fat Navy"]) >= int(key[1]) and int(self.results["Body Fat Navy"]) <= int(key[4]):
+          self.results["Body Fat Category"] = value
+      else:
+        if key == "25%+" and int(self.results["Body Fat Navy"] >= 25): self.results["Body Fat Category"] = value
+        elif int(self.results["Body Fat Navy"]) >= int(key[:2]) and int(self.results["Body Fat Navy"]) <= int(key[3:5]):
+          self.results["Body Fat Category"] = value
+    
+    if self.results["Ideal Body Fat"] == "8.5%":
+      diff_ideal_body_fat = round(self.results["Body Fat Navy"]-float(self.results["Ideal Body Fat"][0:3]), 1)
     else:
-      if self.gender == "male":
-        # U.S. Navy Method
-        si_bfp = 495/(1.0324-0.19077*math.log10(self.waist-self.neck)+0.15456*math.log10(self.height))-450
-        # BMI Method
-        BMI_si = self.weight/self.height**2
-        bmi_bfp = 1.20*BMI_male_si+0.23*self.age-16.2
-      elif self.gender == "female":
-        # U.S. Navy Method
-        si_bfp = 495/(1.29579-0.35994*math.log10(self.waist+self.hip-self.neck)+0.22100*math.log10(self.height))-450
-        # BMI Method
-        BMI_male_si = self.weight/self.height**2
-        bmi_bfp = 1.20*BMI_male_si+0.23*self.age-5.4
+      diff_ideal_body_fat = round(self.results["Body Fat Navy"]-float(self.results["Ideal Body Fat"][0:4]), 1)
+    
+    weight_to_ideal_body_fat = round(fat_mass(diff_ideal_body_fat, self.weight), 1)
+    self.results["Body Fat To Lose To Reach Ideal"] = weight_to_ideal_body_fat
+    
+  def results(self):
+    return self.results
