dac47936ac5c2a76c07687dccfe4762d4ef05e57
Jurica Runtas <runtas.j@gmail.com>
2020-09-18 05:33:34 +0200

    using buttons for muscle group images instead of labels

--- fitness_tracker/notes/workouts/main_panel.py
+++ fitness_tracker/notes/workouts/main_panel.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel, QFrame, QCalendarWidget, QPushButton
-from PyQt5.QtGui import QPixmap, QFont, QIcon, QCursor
-from PyQt5.QtCore import Qt, QFileInfo, QLocale
+from PyQt5.QtGui import QFont, QIcon, QCursor
+from PyQt5.QtCore import Qt, QFileInfo, QLocale, QSize
 
 path = QFileInfo(__file__).absolutePath()
 
@@ -150,43 +150,51 @@ class MainPanel(QWidget):
     first_row = QHBoxLayout()
 
     chest_muscle_group = QVBoxLayout()
-    chest_image = QLabel(self)
-    chest_image.setAlignment(Qt.AlignCenter)
-    chest_pixmap = QPixmap("".join([path, "/muscle_groups/chest_muscles.jpg"]))
-    chest_image.setPixmap(chest_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
-    
+    chest_image = QPushButton(self)
+    chest_image.setIcon(QIcon("".join([path, "/muscle_groups/chest_muscles.jpg"])))
+    chest_image.setIconSize(QSize(100, 60))
+    chest_image.resize(100, 60)
+    chest_image.setStyleSheet("border: none")
+    chest_image.setCursor(Qt.PointingHandCursor)
+
     chest_label = QLabel("Chest", self)
     chest_label.setAlignment(Qt.AlignCenter)
     chest_muscle_group.addWidget(chest_image)
     chest_muscle_group.addWidget(chest_label)
     
     back_muscle_group = QVBoxLayout()
-    back_image = QLabel(self)
-    back_image.setAlignment(Qt.AlignCenter)
-    back_pixmap = QPixmap("".join([path, "/muscle_groups/back_muscles.jpg"]))
-    back_image.setPixmap(back_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
-
+    back_image = QPushButton(self)
+    back_image.setIcon(QIcon("".join([path, "/muscle_groups/back_muscles.jpg"])))
+    back_image.setIconSize(QSize(100, 60))
+    back_image.resize(100, 60)
+    back_image.setStyleSheet("border: none")
+    back_image.setCursor(Qt.PointingHandCursor)
+    
     back_label = QLabel("Back", self)
     back_label.setAlignment(Qt.AlignCenter)
     back_muscle_group.addWidget(back_image)
     back_muscle_group.addWidget(back_label)
     
     triceps_muscle_group = QVBoxLayout()
-    triceps_image = QLabel(self)
-    triceps_image.setAlignment(Qt.AlignCenter)
-    triceps_pixmap = QPixmap("".join([path, "/muscle_groups/triceps_muscles.jpg"]))
-    triceps_image.setPixmap(triceps_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
-    
+    triceps_image = QPushButton(self)
+    triceps_image.setIcon(QIcon("".join([path, "/muscle_groups/triceps_muscles.jpg"])))
+    triceps_image.setIconSize(QSize(100, 60))
+    triceps_image.resize(100, 60)
+    triceps_image.setStyleSheet("border: none")
+    triceps_image.setCursor(Qt.PointingHandCursor)
+
     triceps_label = QLabel("Triceps", self)
     triceps_label.setAlignment(Qt.AlignCenter)
     triceps_muscle_group.addWidget(triceps_image)
     triceps_muscle_group.addWidget(triceps_label)
 
     biceps_muscle_group = QVBoxLayout()
-    biceps_image = QLabel(self)
-    biceps_image.setAlignment(Qt.AlignCenter)
-    biceps_pixmap = QPixmap("".join([path, "/muscle_groups/biceps_muscles.jpg"]))
-    biceps_image.setPixmap(biceps_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
+    biceps_image = QPushButton(self)
+    biceps_image.setIcon(QIcon("".join([path, "/muscle_groups/biceps_muscles.jpg"])))
+    biceps_image.setIconSize(QSize(100, 60))
+    biceps_image.resize(100, 60)
+    biceps_image.setStyleSheet("border: none")
+    biceps_image.setCursor(Qt.PointingHandCursor)
     
     biceps_label = QLabel("Biceps", self)
     biceps_label.setAlignment(Qt.AlignCenter)
@@ -194,10 +202,12 @@ class MainPanel(QWidget):
     biceps_muscle_group.addWidget(biceps_label)
     
     shoulders_muscle_group = QVBoxLayout()
-    shoulders_image = QLabel(self)
-    shoulders_image.setAlignment(Qt.AlignCenter)
-    shoulders_pixmap = QPixmap("".join([path, "/muscle_groups/shoulder_muscles.jpg"]))
-    shoulders_image.setPixmap(shoulders_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
+    shoulders_image = QPushButton(self)
+    shoulders_image.setIcon(QIcon("".join([path, "/muscle_groups/shoulder_muscles.jpg"])))
+    shoulders_image.setIconSize(QSize(100, 60))
+    shoulders_image.resize(100, 60)
+    shoulders_image.setStyleSheet("border: none")
+    shoulders_image.setCursor(Qt.PointingHandCursor)
     
     shoulders_label = QLabel("Shoulders", self)
     shoulders_label.setAlignment(Qt.AlignCenter)
@@ -213,21 +223,25 @@ class MainPanel(QWidget):
     second_row = QHBoxLayout()
     
     core_muscle_group = QVBoxLayout()
-    core_image = QLabel(self)
-    core_image.setAlignment(Qt.AlignCenter)
-    core_pixmap = QPixmap("".join([path, "/muscle_groups/core_muscles.jpg"]))
-    core_image.setPixmap(core_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
-
+    core_image = QPushButton(self)
+    core_image.setIcon(QIcon("".join([path, "/muscle_groups/core_muscles.jpg"])))
+    core_image.setIconSize(QSize(100, 60))
+    core_image.resize(100, 60)
+    core_image.setStyleSheet("border: none")
+    core_image.setCursor(Qt.PointingHandCursor)
+    
     core_label = QLabel("Core", self)
     core_label.setAlignment(Qt.AlignCenter)
     core_muscle_group.addWidget(core_image)
     core_muscle_group.addWidget(core_label)
     
     forearms_muscle_group = QVBoxLayout()
-    forearms_image = QLabel(self)
-    forearms_image.setAlignment(Qt.AlignCenter)
-    forearms_pixmap = QPixmap("".join([path, "/muscle_groups/forearms_muscles.jpg"]))
-    forearms_image.setPixmap(forearms_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
+    forearms_image = QPushButton(self)
+    forearms_image.setIcon(QIcon("".join([path, "/muscle_groups/forearms_muscles.jpg"])))
+    forearms_image.setIconSize(QSize(100, 60))
+    forearms_image.resize(100, 60)
+    forearms_image.setStyleSheet("border: none")
+    forearms_image.setCursor(Qt.PointingHandCursor)
     
     forearms_label = QLabel("Forearms", self)
     forearms_label.setAlignment(Qt.AlignCenter)
@@ -235,10 +249,12 @@ class MainPanel(QWidget):
     forearms_muscle_group.addWidget(forearms_label)
 
     upper_legs_muscle_group = QVBoxLayout()
-    upper_legs_image = QLabel(self)
-    upper_legs_image.setAlignment(Qt.AlignCenter)
-    upper_legs_pixmap = QPixmap("".join([path, "/muscle_groups/upper_legs_muscles.jpg"]))
-    upper_legs_image.setPixmap(upper_legs_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
+    upper_legs_image = QPushButton(self)
+    upper_legs_image.setIcon(QIcon("".join([path, "/muscle_groups/upper_legs_muscles.jpg"])))
+    upper_legs_image.setIconSize(QSize(100, 60))
+    upper_legs_image.resize(100, 60)
+    upper_legs_image.setStyleSheet("border: none")
+    upper_legs_image.setCursor(Qt.PointingHandCursor)
     
     upper_legs_label = QLabel("Upper Legs", self)
     upper_legs_label.setAlignment(Qt.AlignCenter)
@@ -246,10 +262,12 @@ class MainPanel(QWidget):
     upper_legs_muscle_group.addWidget(upper_legs_label)
 
     calves_muscle_group = QVBoxLayout()
-    calves_image = QLabel(self)
-    calves_image.setAlignment(Qt.AlignCenter)
-    calves_pixmap = QPixmap("".join([path, "/muscle_groups/calf_muscles.jpg"]))
-    calves_image.setPixmap(calves_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
+    calves_image = QPushButton(self)
+    calves_image.setIcon(QIcon("".join([path, "/muscle_groups/calf_muscles.jpg"])))
+    calves_image.setIconSize(QSize(100, 60))
+    calves_image.resize(100, 60)
+    calves_image.setStyleSheet("border: none")
+    calves_image.setCursor(Qt.PointingHandCursor)
     
     calves_label = QLabel("Calves", self)
     calves_label.setAlignment(Qt.AlignCenter)
@@ -257,10 +275,12 @@ class MainPanel(QWidget):
     calves_muscle_group.addWidget(calves_label)
     
     cardio_group = QVBoxLayout()
-    cardio_image = QLabel(self)
-    cardio_image.setAlignment(Qt.AlignCenter)
-    cardio_pixmap = QPixmap("".join([path, "/muscle_groups/cardio.png"]))
-    cardio_image.setPixmap(cardio_pixmap.scaled(100, 60, Qt.KeepAspectRatio, Qt.FastTransformation))
+    cardio_image = QPushButton(self)
+    cardio_image.setIcon(QIcon("".join([path, "/muscle_groups/cardio.png"])))
+    cardio_image.setIconSize(QSize(100, 60))
+    cardio_image.resize(100, 60)
+    cardio_image.setStyleSheet("border: none")
+    cardio_image.setCursor(Qt.PointingHandCursor)
     
     cardio_label = QLabel("Cardio", self)
     cardio_label.setAlignment(Qt.AlignCenter)
