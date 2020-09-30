66f412c619bed60458337e9a79908d75a31a4e31
JuricaRT <runtas.j@gmail.com>
2020-09-30 18:47:35 +0200

    added replace grid, added frames, added replace grid functionality to flat barbell bench press button

--- fitness_tracker/notes/workouts/exercises/chest_exercises.py
+++ fitness_tracker/notes/workouts/exercises/chest_exercises.py
@@ -1,17 +1,17 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout, QFrame
 from PyQt5.QtGui import QIcon, QCursor
 from PyQt5.QtCore import QSize, Qt, QFileInfo
+from .exercise import Exercise
 
 path = QFileInfo(__file__).absolutePath()
 
 class ChestExercises(QWidget):
   def __init__(self, parent):
     super().__init__(parent)
+    self.grid = QGridLayout()
     self.create_panel()
     
   def create_panel(self):
-    grid = QGridLayout()
-
     search_bar_layout = QHBoxLayout()
     search_bar_label = QLabel("Search", self)
     search_bar = QLineEdit(self)
@@ -25,7 +25,9 @@ class ChestExercises(QWidget):
     flat_barbell_bp.resize(250, 150)
     flat_barbell_bp.setStyleSheet("border: none")
     flat_barbell_bp.setCursor(Qt.PointingHandCursor)
-    
+    flat_barbell_bp.clicked.connect(lambda: self.replace_grid("Flat Barbell Bench Press"))
+
+
     flat_barbell_bp_label = QLabel("Flat Barbell Bench Press", self)
     flat_barbell_bp_label.setAlignment(Qt.AlignCenter)
     flat_barbell_bp_layout.addWidget(flat_barbell_bp)
@@ -38,7 +40,7 @@ class ChestExercises(QWidget):
     flat_dumbbell_bp.resize(250, 150)
     flat_dumbbell_bp.setStyleSheet("border: none")
     flat_dumbbell_bp.setCursor(Qt.PointingHandCursor)
-
+    
     flat_dumbbell_bp_label = QLabel("Flat Dumbbell Bench Press", self)
     flat_dumbbell_bp_label.setAlignment(Qt.AlignCenter)
     flat_dumbbell_bp_layout.addWidget(flat_dumbbell_bp)
@@ -95,12 +97,33 @@ class ChestExercises(QWidget):
     machine_chest_fly_label.setAlignment(Qt.AlignCenter)
     machine_chest_fly_layout.addWidget(machine_chest_fly)
     machine_chest_fly_layout.addWidget(machine_chest_fly_label)
+    
+    frame = QFrame()
+    frame1 = QFrame()
+    frame2 = QFrame()
+    frame3 = QFrame()
+    frame4 = QFrame()
+    frame5 = QFrame()
+    frame6 = QFrame()
+    
+    frame.setLayout(search_bar_layout)
+    frame1.setLayout(flat_barbell_bp_layout)
+    frame2.setLayout(flat_dumbbell_bp_layout)
+    frame3.setLayout(incline_dumbbell_bp_layout)
+    frame4.setLayout(incline_barbell_bp_layout)
+    frame5.setLayout(push_ups_layout)
+    frame6.setLayout(machine_chest_fly_layout)
 
-    grid.addLayout(search_bar_layout, 0, 0, 1, 4)
-    grid.addLayout(flat_barbell_bp_layout, 1, 0)
-    grid.addLayout(flat_dumbbell_bp_layout, 1, 1)
-    grid.addLayout(incline_dumbbell_bp_layout, 1, 2)
-    grid.addLayout(incline_barbell_bp_layout, 2, 0)
-    grid.addLayout(push_ups_layout, 2, 1)
-    grid.addLayout(machine_chest_fly_layout, 2, 2)
-    self.setLayout(grid)
+    self.grid.addWidget(frame, 0, 0, 1, 4)
+    self.grid.addWidget(frame1, 1, 0)
+    self.grid.addWidget(frame2, 1, 1)
+    self.grid.addWidget(frame3, 1, 2)
+    self.grid.addWidget(frame4, 2, 0)
+    self.grid.addWidget(frame5, 2, 1)
+    self.grid.addWidget(frame6, 2, 2)
+    self.setLayout(self.grid)
+
+  def replace_grid(self, exercise):
+    for i in reversed(range(self.grid.count())):
+      self.grid.itemAt(i).widget().deleteLater()
+    self.grid.addWidget(Exercise(self, exercise, "Chest Exercises"), 0, 0)
