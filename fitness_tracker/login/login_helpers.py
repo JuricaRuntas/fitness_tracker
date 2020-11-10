29789d045268081c97864063531b43901a76c52a
JuricaRT <runtas.j@gmail.com>
2020-11-10 20:42:06 +0100

    update and refactor

--- fitness_tracker/login/login_helpers.py
+++ fitness_tracker/login/login_helpers.py
@@ -5,7 +5,6 @@ import sqlite3
 from psycopg2 import sql
 from PyQt5.QtCore import QFileInfo
 
-# path for sqlite3 user info database
 path = os.path.normpath(QFileInfo(__file__).absolutePath())
 db_path = path.split(os.path.sep)[:-2]
 db_path = os.path.sep.join([os.path.sep.join(db_path), "db", "user_info.db"])
@@ -13,61 +12,53 @@ db_path = os.path.sep.join([os.path.sep.join(db_path), "db", "user_info.db"])
 db_info = {"host": "fitnesstracker.cc7s2r4sjjv6.eu-west-3.rds.amazonaws.com", "port": 5432,
            "database": "postgres", "user": "admin", "password": "admin"}
 
-def check_password(email, password):
-  status = True
-  query = "SELECT * FROM users WHERE email=%s"
-  with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
-                        user=db_info["user"], password=db_info["password"]) as conn:
-    with conn.cursor() as cursor:
-      cursor.execute(query, (email,))
-      database_password = cursor.fetchone()[1]
-      if not hashlib.sha256(password.encode('UTF-8')).hexdigest() == database_password: status = False
-  return status
-
-def get_table_name(email, password):
-  table_name = None
-  if check_password(email, password):
+class Login():
+  def check_password(self, email, password):
+    status = True
     query = "SELECT * FROM users WHERE email=%s"
     with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
                           user=db_info["user"], password=db_info["password"]) as conn:
       with conn.cursor() as cursor:
         cursor.execute(query, (email,))
-        table_name = cursor.fetchone()[2]
-  return table_name
+        database_password = cursor.fetchone()[1]
+        if not hashlib.sha256(password.encode('UTF-8')).hexdigest() == database_password: status = False
+    return status
 
-def get_user_info(email, password):
-  table_name = get_table_name(email, password)
-  columns = None
-  user_info = None
-
-  with sqlite3.connect(db_path) as conn:
-    table_exists = "SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';"
-    sqlite_cursor = conn.cursor()
-    sqlite_cursor.execute(table_exists.format(table=table_name))
-    if sqlite_cursor.fetchone() == None: # table doesn't exist
-      with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
+  def get_user_info(self, email, password):
+    table_name = "".join([email, "_table"])
+    columns = None
+    user_info = None
+    with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
                           user=db_info["user"], password=db_info["password"]) as conn:
         with conn.cursor() as cursor:
           # this can be changed to fetch something else later on
-          cursor.execute(sql.SQL("SELECT * FROM {table}").format(table=sql.Identifier(table_name)))
-          user_info = cursor.fetchone()[1:]
-          query = """
-                  SELECT column_name FROM information_schema.columns 
-                  WHERE table_name = %s;
-                  """
-          cursor.execute(query, (table_name,))
+          cursor.execute(("SELECT * FROM users WHERE email='{email}'").format(email=email))
+          user_info = cursor.fetchone()[:-1]
           # fetch all column names
           # change this in the future
-          columns = tuple(value[0] for value in cursor.fetchall()[1:])
-      create_table = """
-                     CREATE TABLE "{table}" (
-                     ID integer NOT NULL,
-                     name text NOT NULL,
-                     gender text NOT NULL,
-                     units text NOT NULL,
-                     weight real NOT NULL,
-                     PRIMARY KEY (ID));
-                     """
-      sqlite_cursor.execute(create_table.format(table=table_name))
-      insert_values = "INSERT INTO '{table}' {columns} VALUES {values}"
-      sqlite_cursor.execute(insert_values.format(table=table_name, columns=columns, values=user_info))
+          query = """
+                  SELECT column_name FROM information_schema.columns 
+                  WHERE table_name = 'users';
+                  """
+          cursor.execute(query)
+          columns = tuple(value[0] for value in cursor.fetchall() if not value[0] == 'id')
+    
+    with sqlite3.connect(db_path) as conn:
+      table_exists = "SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';"
+      cursor = conn.cursor()
+      cursor.execute(table_exists.format(table=table_name))
+      if cursor.fetchone() == None: # table doesn't exist
+        create_table = """
+                       CREATE TABLE "{table}" (
+                       email text NOT NULL,
+                       password text NOT NULL,
+                       name text NOT NULL,
+                       gender text NOT NULL,
+                       units text NOT NULL,
+                       weight real NOT NULL,
+                       ID integer NOT NULL,
+                       PRIMARY KEY (ID));
+                       """
+        cursor.execute(create_table.format(table=table_name))
+        insert_values = "INSERT INTO '{table}' {columns} VALUES {values}"
+        cursor.execute(insert_values.format(table=table_name, columns=columns, values=user_info))
--- fitness_tracker/signup/signup_helpers.py
+++ fitness_tracker/signup/signup_helpers.py
@@ -7,7 +7,6 @@ import psycopg2
 from psycopg2 import sql
 from PyQt5.QtCore import QFileInfo
 
-# path for sqlite3 user info database
 path = os.path.normpath(QFileInfo(__file__).absolutePath())
 db_path = path.split(os.path.sep)[:-2]
 db_path = os.path.sep.join([os.path.sep.join(db_path), "db", "user_info.db"])
@@ -15,84 +14,107 @@ db_path = os.path.sep.join([os.path.sep.join(db_path), "db", "user_info.db"])
 db_info = {"host": "fitnesstracker.cc7s2r4sjjv6.eu-west-3.rds.amazonaws.com", "port": 5432, 
            "database": "postgres", "user": "admin", "password": "admin"}
 
-def check_valid_email(email):
-  # basic validation, checks for example@gmail.com and similar format
-  # for real validation, email confirmation is needed
-  email_regex = re.compile(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,6}$")
-  return not re.match(email_regex, email) == None
- 
-def check_valid_password(password):
-  valid = True
-  valid_characters = set(string.ascii_letters + string.digits + '@#$%^&+=')
-  if (len(password) < 8):
-    valid = False
-    print("Password is too short.")
-  elif any(char not in valid_characters for char in password):
-    valid = False
-    print("Password contains invalid characters.")
-  return valid
+class Signup:
+  def check_valid_email(self, email):
+    # basic validation, checks for example@gmail.com and similar format
+    # for real validation, email confirmation is needed
+    email_regex = re.compile(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,6}$")
+    return not re.match(email_regex, email) == None
+   
+  def check_valid_password(self, password):
+    valid = True
+    valid_characters = set(string.ascii_letters + string.digits + '@#$%^&+=')
+    if (len(password) < 8):
+      valid = False
+      print("Password is too short.")
+    elif any(char not in valid_characters for char in password):
+      valid = False
+      print("Password contains invalid characters.")
+    return valid
 
-def get_table_name():
-  table_name = None
-  with sqlite3.connect(db_path) as conn:
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'")
-    try:
-      # returns table name if exists otherwise returns None
-      table_name = cursor.fetchone()[0]
-    except TypeError: # catch TypeError caused by subscripting 'NoneType' object
-      pass
-  return table_name
+  def fetch_user_email(self):
+    email = None
+    with sqlite3.connect(db_path) as conn:
+      cursor = conn.cursor()
+      table_name = self.fetch_table_name()
+      cursor.execute("SELECT email FROM '{table}'".format(table=table_name))
+      try:
+        email = cursor.fetchone()[0]
+      except TypeError:
+        pass
+    return email
 
-def create_user(user_email, user_password):
-  status = True
-  table_name = "".join([user_email, "_table"])
-  with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
-                        user=db_info["user"], password=db_info["password"]) as conn:
-    try:
+  def fetch_table_name(self):
+    table_name = None
+    with sqlite3.connect(db_path) as conn:
+      cursor = conn.cursor()
+      cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'")
+      try:
+        # returns table name if exists otherwise returns None
+        table_name = cursor.fetchone()[0]
+      except TypeError: # catch TypeError caused by subscripting 'NoneType' object
+        pass
+    return table_name
+
+  def create_user(self, user_email, user_password):
+    self.email = user_email
+    status = True
+    #table_name = "".join([user_email, "_table"])6
+    with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
+                          user=db_info["user"], password=db_info["password"]) as conn:
+      try:
+        with conn.cursor() as cursor:
+          user_password = hashlib.sha256(user_password.encode('utf-8')).hexdigest()
+          query = "INSERT INTO users (email, password) VALUES (%s, %s)"
+          cursor.execute(query, (user_email, user_password,))
+      except psycopg2.errors.UniqueViolation:
+        status = False
+        print("User with '%s' email address already exists.") % user_email
+    return status
+
+  def create_user_info_after_signup(self, user_info, email):
+    table_name = self.fetch_table_name()
+    update_query = "UPDATE users SET"
+    with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
+                          user=db_info["user"], password=db_info["password"]) as conn:
       with conn.cursor() as cursor:
-        user_password = hashlib.sha256(user_password.encode('utf-8')).hexdigest()
-        query = "INSERT INTO users VALUES (%s, %s, %s)"
-        cursor.execute(query, (user_email, user_password, table_name,))
-    except psycopg2.errors.UniqueViolation:
-      status = False
-      print("User with '%s' email address already exists.") % user_email
-  return status
+        columns = tuple(user_info.keys())
+        values = tuple(user_info.values())
+        pairs = [[pair[0], pair[1]] for pair in zip(columns, values)]
+        for pair in pairs:
+          pair[1] = "'%s'" % pair[1]
+          joined_pair = "=".join([pair[0], pair[1]])
+          if pairs.index(pair) == 0: update_query = " ".join([update_query, joined_pair])
+          elif pairs.index(pair) == len(pairs)-1:
+            update_query = ", ".join([update_query, joined_pair])
+            update_query = " ".join([update_query, "WHERE email=%s"])
+          else: update_query = ", ".join([update_query, joined_pair])
+        cursor.execute(update_query, (email,))
+      
+      update_query = update_query.replace("users", "'%s'").replace("email=%s", "email='%s'")
+      update_query = update_query % (table_name, email)
+      with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute(update_query)
 
-def create_user_table(email):
-  status = True
-  table_name = "".join([email, "_table"])
-  # set user table columns here
-  create_table = """
-                 CREATE TABLE {table} (
-                 ID bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 99999999 ),
-                 name text NOT NULL,
-                 gender text NOT NULL,
-                 units text NOT NULL,
-                 weight real NOT NULL,
-                 PRIMARY KEY (ID));
-                 """
-  with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
-                        user=db_info["user"], password=db_info["password"]) as conn:
-    with conn.cursor() as cursor:
-      cursor.execute(sql.SQL(create_table).format(table=sql.Identifier(table_name)))
-  
-  diff_id = "bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 99999999 )"
-  create_table = create_table.replace(diff_id, "integer NOT NULL").replace("{table}", '"%s"') % table_name
-  with sqlite3.connect(db_path) as conn:
-    cursor = conn.cursor()
-    cursor.execute(create_table) 
-
-def create_user_info_after_signup(user_info, user_table_name):
-  query = "INSERT INTO {table} ({columns}) VALUES %s"
-  with psycopg2.connect(host=db_info["host"], port=db_info["port"], database=db_info["database"],
-                        user=db_info["user"], password=db_info["password"]) as conn: 
-    with conn.cursor() as cursor:  
-      columns = sql.SQL(", ").join(sql.Identifier(column) for column in tuple(user_info.keys()))
-      values = tuple(value for value in user_info.values())
-      cursor.execute(sql.SQL(query).format(table=sql.Identifier(user_table_name), columns=columns), (values,))
-  
-  sqlite_query = """INSERT INTO "{}" {columns} VALUES {values}"""
-  with sqlite3.connect(db_path) as conn:
-    cursor = conn.cursor()
-    cursor.execute(sqlite_query.format(user_table_name, columns=tuple(user_info.keys()), values=tuple(user_info.values())))
+  def create_user_table(self, email, password):
+    status = True
+    table_name = "".join([email, "_table"])
+    # set user table columns here
+    create_table = """
+                   CREATE TABLE '%s' (
+                   email text NOT NULL,
+                   password text NOT NULL,
+                   name text,
+                   gender text,
+                   units text,
+                   weight real,
+                   id integer NOT NULL,
+                   PRIMARY KEY (ID));
+                   """ % table_name
+                   
+    with sqlite3.connect(db_path) as conn:
+      cursor = conn.cursor()
+      cursor.execute(create_table)
+      insert_email = "INSERT INTO '%s'(email, password) VALUES ('%s', '%s')" % (table_name, email, password)
+      cursor.execute(insert_email)
