bf746d70c15a7cd34abf9d688406cc7411b7b12b
JuricaRT <runtas.j@gmail.com>
2020-11-11 19:44:39 +0100

    added functions for main panel windows, main panel is now fetching lifts and units

--- fitness_tracker/notes/big_lifts/main_panel.py
+++ fitness_tracker/notes/big_lifts/main_panel.py
@@ -2,10 +2,22 @@ from PyQt5.QtWidgets import QPushButton, QGridLayout, QVBoxLayout, QWidget, QLab
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 from .preferred_lifts import PreferredLifts
+from .update_1RM_window import Update1RMWindow
+from .update_lifts_for_reps_window import UpdateLiftsForRepsWindow
+from .lift_history import LiftHistory
+from .big_lifts_helpers import BigLifts
 
 class MainPanel(QWidget):
   def __init__(self, parent):
     super().__init__(parent)
+    self.interface = BigLifts()
+    self.interface.create_big_lifts_table()
+    if self.interface.table_is_empty():
+      exercises = PreferredLifts().fetch_current_exercises()
+      self.interface.insert_default_values(exercises)
+    self.units = "kg" if self.interface.fetch_units() == "metric" else "lb"
+    self.one_RM = [[lift, weight, self.units] for lift, weight in self.interface.fetch_one_rep_maxes().items()]
+    self.lifts_reps = [[lift, weight, self.units] for lift, weight in self.interface.fetch_lifts_for_reps().items()]
     self.create_panel()
 
   def create_panel(self):
@@ -20,7 +32,6 @@ class MainPanel(QWidget):
     panel_description = QVBoxLayout()
     description_label = QLabel("Keep notes and track progress of your preferred big lifts here.", self)
     description_label.setFixedHeight(30)
-
     panel_description.addWidget(description_label)
     return panel_description
 
@@ -51,20 +62,21 @@ class MainPanel(QWidget):
     main_label = QLabel("One Rep Max")
     main_label.setFont(QFont("Ariel", 18, weight=QFont.Bold))
 
-    horizontal_press_label = QLabel("Bench Press: 80 kg")
+    horizontal_press_label = QLabel(": ".join(self.one_RM[0]))
     horizontal_press_label.setFont(QFont("Ariel", 10))
 
-    floor_pull_label = QLabel("Deadlift: 140 kg")
+    floor_pull_label = QLabel(": ".join(self.one_RM[1]))
     floor_pull_label.setFont(QFont("Ariel", 10))
 
-    squat_label = QLabel("Front Squat: 90 kg")
+    squat_label = QLabel(": ".join(self.one_RM[2]))
     squat_label.setFont(QFont("Ariel", 10))
 
-    vertical_press_label = QLabel("Military Press: 55 kg")
+    vertical_press_label = QLabel(": ".join(self.one_RM[3]))
     vertical_press_label.setFont(QFont("Ariel", 10))
 
     orm_buttons = QHBoxLayout()
     update_button = QPushButton("Update")
+    update_button.clicked.connect(self.create_update_1RM_window)
     clear_button = QPushButton("Clear")
     orm_buttons.addWidget(update_button)
     orm_buttons.addWidget(clear_button)
@@ -88,20 +100,21 @@ class MainPanel(QWidget):
     main_label = QLabel("Lifts For Reps")
     main_label.setFont(QFont("Ariel", 18, weight=QFont.Bold))
 
-    horizontal_press_label = QLabel("Bench Press: 70 kg, 4 reps")
+    horizontal_press_label = QLabel(": ".join(self.lifts_reps[0]))
     horizontal_press_label.setFont(QFont("Ariel", 10))
 
-    floor_pull_label = QLabel("Deadlift: 120 kg, 5 reps")
+    floor_pull_label = QLabel(": ".join(self.lifts_reps[1]))
     floor_pull_label.setFont(QFont("Ariel", 10))
 
-    squat_label = QLabel("Front Squat: 70 kg, 7 reps")
+    squat_label = QLabel(": ".join(self.lifts_reps[2]))
     squat_label.setFont(QFont("Ariel", 10))
 
-    vertical_press_label = QLabel("Military Press: 50 kg, 2 reps")
+    vertical_press_label = QLabel(": ".join(self.lifts_reps[3]))
     vertical_press_label.setFont(QFont("Ariel", 10))
 
     reps_buttons = QHBoxLayout()
     update_button = QPushButton("Update")
+    update_button.clicked.connect(self.create_update_lifts_for_reps_window)
     clear_button = QPushButton("Clear")
     reps_buttons.addWidget(update_button)
     reps_buttons.addWidget(clear_button)
@@ -123,6 +136,7 @@ class MainPanel(QWidget):
     buttons_panel = QHBoxLayout()
     lift_history_button = QPushButton()
     lift_history_button.setText("Lift History")
+    lift_history_button.clicked.connect(self.create_lift_history_window)
     preferred_lists_button = QPushButton()
     preferred_lists_button.setText("Preferred Lifts")
     preferred_lists_button.clicked.connect(self.create_preferred_lists_window)
@@ -134,8 +148,22 @@ class MainPanel(QWidget):
   def create_preferred_lists_window(self):
     global plists_window
     plists_window = PreferredLifts()
-    plists_window.setGeometry(100, 200, 300, 300)
     plists_window.show()
 
+  def create_lift_history_window(self):
+    global lift_history_window
+    lift_history_window = LiftHistory()
+    lift_history_window.setGeometry(100, 200, 300, 300)
+    lift_history_window.show()
 
+  def create_update_1RM_window(self):
+    global update_1RM_window
+    update_1RM_window = Update1RMWindow()
+    update_1RM_window.setGeometry(100, 200, 300, 300)
+    update_1RM_window.show()
 
+  def create_update_lifts_for_reps_window(self):
+    global lifts_for_reps
+    lifts_for_reps = UpdateLiftsForRepsWindow()
+    lifts_for_reps.setGeometry(100, 200, 300, 300)
+    lifts_for_reps.show()
