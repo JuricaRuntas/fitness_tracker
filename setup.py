45b1b92cc1650eac6d801861f0be8ad4f6ce0c97
JuricaRT <runtas.j@gmail.com>
2021-01-17 09:18:05 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
   long_description_content_type="text/markdown",
   packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
   include_package_data=True,
-  classifiers=["Programming Langugage :: Python :: 3",
+  classifiers=["Programming Language :: Python :: 3",
                "License :: OSI Approved :: MIT License",
                "Operating System :: OS Independent"],
   install_requires=["PyQt5", "requests", "psycopg2",
